76e9d2ae801e8b685464999db846d2937175c843
Alexander Nolte <alexander.nolte@ut.ee>
2023-07-05 16:15:24 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# genaihackathon2023.github.io
+# Generative AI Innovation Incubator Hackathon
 
-Testsite
+This repository contains the website for the Generative AI Innovation Incubator Hackathon
+
+[**Visit the website**](https://genaihackathon2023.github.io/)
+
+
+All contributions to this repository will be released under the [Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0/). By submitting a pull request, you are agreeing to comply with this waiver of copyright interest. You are free to share and adapt the content here but you are required to give appropriate credit to the original content and share your content under the same lincense.
---
+++ js/scripts.js
@@ -0,0 +1,101 @@
+/*!
+    * Start Bootstrap - Agency v6.0.0 (https://startbootstrap.com/template-overviews/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
+    */
+    function resizeHeader() {
+        var paddingValue = Math.round(($(window).height()-$('#header-container').height())/2);
+        if (paddingValue <= 115) {
+            paddingValue = 115;
+        }
+        $('header').css({ 'height': $(window).height() });
+        $('#header').css({ 'padding-top': paddingValue+'px' });
+    };
+
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    //ensure that header image is always maxed and the content is centered
+    resizeHeader();
+
+    $(window).on('resize', function(){
+        resizeHeader();        
+    });
+
+    //enabling the back-button for modal close but only when online
+    /*switch(window.location.protocol) {
+        case 'file:':
+            break;
+        default:
+            $('.modal').on('shown.bs.modal', function() {
+                if (typeof(this.dataset.hash) !== 'undefined') {
+                    history.pushState(null, null, this.dataset.hash);
+                }
+            });
+
+            $('.modal').on('hide.bs.modal', function(event) {
+                if (this.dataset.pushback !== 'true') {
+                    event.preventDefault();
+                    history.back();
+                }
+                this.dataset.pushback = '';
+            });
+
+            window.onpopstate = function() {
+                let open_modal = document.querySelector('.modal.show');
+                if (open_modal) {
+                    open_modal.dataset.pushback = 'true';
+                    $(open_modal).modal('hide');
+                };
+            };
+    }*/
+
+})(jQuery); // End of use strict
